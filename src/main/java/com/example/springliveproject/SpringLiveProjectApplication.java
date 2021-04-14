55c651e694e5fb136286915d1dddbe8dec115cc1
ckeller22 <chriskeller222@gmail.com>
2021-04-14 10:44:35 -0700

    initial commit

---
+++ src/main/java/com/example/springliveproject/SpringLiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springliveproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLiveProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLiveProjectApplication.class, args);
+    }
+
+}
