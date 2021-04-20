36661b3dfe8e7b932003731fe0e5821f8810c678
ckeller22 <chriskeller222@gmail.com>
2021-04-19 21:57:45 -0700

    Milestone one, establish simulated API endpoint.

--- src/test/java/com/example/springliveproject/TransactionComponentTest.java
+++ src/test/java/com/example/springliveproject/TransactionComponentTest.java
@@ -4,22 +4,24 @@ import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
 import static io.restassured.module.mockmvc.matcher.RestAssuredMockMvcMatchers.*;
 
 
-@SpringBootTest
+
 public class TransactionComponentTest {
+
+    @LocalServerPort
+    private int port;
 
     @Test
     public void testApplicationEndToEnd() {
 
-        // establish base uri default in before test, before class extension
-
         given().standaloneSetup(new TransactionController(new TransactionService()))
                 .when()
-                .get("/transactions/1234567")
+                .get(String.format("http://localhost:%s/transactions/1234567", port))
                 .then()
                 .statusCode(Matchers.is(200));
 
